d37a9ca718d7c1630994b7d50a35733563e3d9f6
patexo <patexowin@hotmail.es>
2021-08-10 20:04:26 +0200

    Step 10.0: Deployment at Heroku.

--- models/index.js
+++ models/index.js
@@ -7,6 +7,8 @@ const Sequelize = require('sequelize');
 // Environment variable to define the URL of the data base to use.
 // To use SQLite data base:
 //    DATABASE_URL = sqlite:instant.sqlite
+// To use  Heroku Postgres data base:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
 const url = process.env.DATABASE_URL || "sqlite:instant.sqlite";
 
 const sequelize = new Sequelize(url);
